4e45abaa25784da656761d6eb47602b093551348
TheDutchMC04 <tobias64a@gmail.com>
2019-03-21 20:57:00 +0100

    Finished (for now) init UI, added Connected UI

--- src/client/ClientGui.java
+++ src/client/ClientGui.java
@@ -1,19 +1,16 @@
 package client;
 
-import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-
 public class ClientGui {
-
+	
+	boolean stateInit = false;
+	boolean stateConnected = true;
+	
 	public void init() {
-		
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		
+			
 		//Frame init
 		JFrame frame = new JFrame();
 		frame.setTitle("VideoAD Client");
@@ -23,20 +20,32 @@ public class ClientGui {
 		frame.getContentPane().setLayout(null);		
 		frame.setVisible(true);
 		
-		int w = frame.getSize().width;
-		int h = frame.getSize().height;
-				
-		//Labels
-		JLabel splashText = new JLabel("RemoteAD", SwingConstants.CENTER);
-		splashText.setFont(new Font("Serif", Font.BOLD, 60));
-		splashText.setBounds((dim.width-splashText.getSize().width)/2, 15, 300, 80);
-		frame.getContentPane().add(splashText);
+		int w = frame.getWidth();
+		int h = frame.getHeight();
 		
-		JLabel subSplashText = new JLabel("Initializing");
-		subSplashText.setFont(new Font("Serif", Font.BOLD, 30));
-		subSplashText.setBounds(322, 11, 192, 64);
-
-		frame.getContentPane().add(subSplashText);
-
+		//Labels
+		
+		//If in Initialization state
+		if(stateInit) {
+			JLabel splashText = new JLabel("RemoteAD");
+			splashText.setFont(new Font("Serif", Font.BOLD, 60));
+			splashText.setBounds(w / 2 - splashText.getPreferredSize().width / 2, h / 2 - splashText.getPreferredSize().height / 2, splashText.getPreferredSize().width, splashText.getPreferredSize().height);
+			frame.getContentPane().add(splashText);
+			
+			JLabel state = new JLabel("Initializing");
+			state.setFont(new Font("Serif", Font.BOLD, 30));
+			state.setBounds(w / 2 - state.getPreferredSize().width / 2, h / 2 - state.getPreferredSize().height + splashText.getPreferredSize().height + 5, state.getPreferredSize().width, state.getPreferredSize().height);
+			frame.getContentPane().add(state);
+		} else if(stateConnected) { //If Connected to Server
+			JLabel state = new JLabel("Connected to server");
+			state.setFont(new Font("Serif", Font.PLAIN, 25));
+			state.setBounds(10, 10, state.getPreferredSize().width, state.getPreferredSize().height);
+			frame.getContentPane().add(state);
+			
+			JLabel waitingForTrigger = new JLabel("Waiting for Sensor Trigger");
+			waitingForTrigger.setFont(new Font("Serif", Font.PLAIN, 15));
+			waitingForTrigger.setBounds(10, state.getPreferredSize().height + 10, waitingForTrigger.getPreferredSize().width, waitingForTrigger.getPreferredSize().height);
+			frame.getContentPane().add(waitingForTrigger);
+		}
 	}
 }

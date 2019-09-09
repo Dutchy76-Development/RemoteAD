a135e88259fe26a52d5a6c65df4237515d4a6ae3
Tobias de Bruijn <github@thedutchmc.nl>
2019-09-09 15:57:56 +0200

    cleanup

--- src/server/Clients.java
+++ src/server/Clients.java
@@ -14,6 +14,7 @@ public class Clients {
 		this.enabled = enabled;
 	}
 	
+	
 	//Socket
 	public Socket getSocket() {
 		return this.socket;
--- src/server/ServerSocketHandler.java
+++ src/server/ServerSocketHandler.java
@@ -6,15 +6,12 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class ServerSocketHandler {
 	
 	//List<Clients> clients = new ArrayList<Clients>();
 	List<Socket> clientsSocket = new ArrayList<Socket>();
-	HashMap<Socket, Integer> clientsHash = new HashMap<Socket, Integer>();
-	HashMap<Integer, Socket> finalClientRegister = new HashMap<Integer, Socket>();
 	Socket newClientSocket;
 	ServerSocket serverSocket = null;
 
@@ -38,23 +35,10 @@ public class ServerSocketHandler {
 				synchronized(serverSocket) {
 					newClientSocket = serverSocket.accept();
 					clientsSocket.add(newClientSocket);
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 					ClientConnector connector = new ClientConnector(this, newClientSocket);
 					connector.start();
-					this.run();
-=======
-=======
->>>>>>> 297275866751761321b6922181ba7e98c8829c02
-					
-					//Put the client in Hashmap
-					clientsHash.put(newClientSocket, 0);
-					System.err.println(clientsHash.get(newClientSocket));
-					
-<<<<<<< HEAD
->>>>>>> 297275866751761321b6922181ba7e98c8829c02
-=======
->>>>>>> 297275866751761321b6922181ba7e98c8829c02
+					this.run();					
 				}
 			}
 			
@@ -64,15 +48,14 @@ public class ServerSocketHandler {
 		}
 	}
 	
-<<<<<<< HEAD
-=======
+
 	public void removeClient(Socket socket) {
 		synchronized(this) {
 			//clients.remove(socket);
 		}
 	}
 	
->>>>>>> 297275866751761321b6922181ba7e98c8829c02
+
 	//Connect Client & handle messages
 	class ClientConnector extends Thread {
 		private Socket socket;
@@ -92,15 +75,13 @@ public class ServerSocketHandler {
 				in = new DataInputStream(this.socket.getInputStream()); // create input stream for listening for incoming messages
 				out = new DataOutputStream(this.socket.getOutputStream());
 			} catch (IOException e) {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 				System.err.println("[ServerSocketHandler] Failed to open input stream");
-=======
+
 				System.err.println("Failed to open input or output stream");
->>>>>>> 297275866751761321b6922181ba7e98c8829c02
-=======
+
 				System.err.println("Failed to open input or output stream");
->>>>>>> 297275866751761321b6922181ba7e98c8829c02
+
 				e.printStackTrace(System.err);
 				return;
 			}
@@ -121,71 +102,34 @@ public class ServerSocketHandler {
 				if(msg_rec != null) {    
 					String[] msgReceivedArray = msg_rec.split(",");
 
-					int uuid = 0;
+					int id = 0;
 					
 					try {
-<<<<<<< HEAD
-						uuid = Integer.valueOf(msgReceivedArray[0]);
-=======
-						id = Integer.valueOf(idAsString);
-						clientsHash.put(socket, id);
-						System.out.println("ClientsHash: " + clientsHash.get(socket));
+						id = Integer.valueOf(msgReceivedArray[0]);						
 						
-						finalClientRegister.put(id, socket);
-						System.out.println("finalClientRegister Entry: " + finalClientRegister.get(id));
-<<<<<<< HEAD
->>>>>>> 297275866751761321b6922181ba7e98c8829c02
-=======
->>>>>>> 297275866751761321b6922181ba7e98c8829c02
 					} catch (Exception e) {
 						System.err.println("[ServerSocketHandler] Unable to grab UUID, Does the UUID consist of numbers, and only numbers?");
 					}
-					
-<<<<<<< HEAD
-<<<<<<< HEAD
-					String message = msgReceivedArray[1];
-					
-					clients.add(new Clients(uuid, newClientSocket, true));
-					System.out.println("[ServerSocketHandler] Client " + socket.getInetAddress().getHostName() + " Has UUID: " + uuid);
-					System.out.println("[ALPHA] " + getClientBySocket(socket));
+					new Clients(id, newClientSocket, true);
+					System.out.println("[ServerSocketHandler] Client " + socket.getInetAddress().getHostName() + " Has ID: " + id);
 					
 					System.out.println("[ServerSocketHandler] Starting new listener...");
-=======
->>>>>>> 297275866751761321b6922181ba7e98c8829c02
-					
-=======
-					
->>>>>>> 297275866751761321b6922181ba7e98c8829c02
-					String message = msg_rec_array[1];
+
+					String message = msgReceivedArray[1];
 										
 					if(message.equalsIgnoreCase("reqKey")) {
 						//TODO: KeyFinder
 						
-<<<<<<< HEAD
-<<<<<<< HEAD
 						System.out.println("[ALPHA] Request Key from database");
-=======
-=======
->>>>>>> 297275866751761321b6922181ba7e98c8829c02
+
 						// A test (TEMP CODE)
 						ServerSocketSender SSS = new ServerSocketSender();
 						SSS.sendMessage("This will be a key", socket);
 						//END OF TEST CODE
 						
-						System.out.println("Request Key from database");
->>>>>>> 297275866751761321b6922181ba7e98c8829c02
 					}
 				}
 			}
  		} 
 	}
-	
-	//TEMP
-	/*public static Clients getClientBySocket(Socket socket) {
-		for(Clients client : clients) {
-			if(client.getSocket() == socket) {
-				return client;
-			}
-		} return null;
-	}*/
 }

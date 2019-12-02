44c4d1e0ed8260606000d0ee5246b43031ffac21
TheDutchMC04 <tobias64a@gmail.com>
2019-12-02 22:31:58 +0100

    more restructuring, removed gui for server

--- src_server/nl/thedutchmc/remotead/server/Server.java
+++ src_server/nl/thedutchmc/remotead/server/Server.java
@@ -14,11 +14,6 @@ public class Server {
 
 	public static void main(String[] args) {
 		
-		//Start the GUI
-	//	ServerGui gui = new ServerGui();
-	//	gui.init();
-		
-		
 		//Start the SocketHandler and get server online 
 		final ServerSocketHandler handler = new ServerSocketHandler();
 		handler.startServer();
